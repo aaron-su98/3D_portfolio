f0e4825334bc0b2d04c4d71f577a122cf6ea2a37
aaron-su98 <mr.aaron.su@gmail.com>
2023-03-13 03:34:58 -0400

    Initial commit, hero page, 3D computer model render

---
+++ src/assets/index.js
@@ -0,0 +1,60 @@
+import logo from "./logo.png";
+import favicon from "./favicon.png";
+import backend from "./backend.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import postgresql from "./tech/postgresql.png";
+import expressjs from "./tech/expressjs.png";
+import sequelize from "./tech/sequelize.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  favicon,
+  backend,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  threejs,
+  postgresql,
+  expressjs,
+  sequelize,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
